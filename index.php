88426d7bbc8bb7f0f8aaebc531d959d48cdf87f9
robertocapah <roberto_capah@yahoo.com>
2016-09-21 12:02:29 +0700

    beta 1

--- index.php
+++ index.php
@@ -45,7 +45,22 @@
 	echo "<a href='?page=trapesium'>hitung Trapesium </a>";
 	echo "</br>";
 	echo "<a href='?page=lingkaran'>hitung Lingkaran </a>";
-    
+    echo "</br>";
+    echo "</br>";
+    echo "</br>";
+    echo "Bangun Ruang";
+    echo "</br>";
+    echo "<a href='?page=bola'>hitung Bola</a>";
+	echo "</br>";
+	echo "<a href='?page=kerucut'>hitung Kerucut </a>";
+	echo "</br>";
+	echo "<a href='?page=kubus'>hitung Kubus </a>";
+	echo "</br>";
+	echo "<a href='?page=limasSegiEmpat'>hitung Limas Segi Empat </a>";
+	echo "</br>";
+	echo "<a href='?page=prismaSegitiga'>hitung PrismaSegitiga </a>";
+	echo "</br>";
+	echo "<a href='?page=tabung'>hitung Tabung </a>";
      function TampilkanFormA()
 	  {
 	    echo '<form method="POST">';
@@ -67,14 +82,7 @@
         echo '<form method="POST">';
         echo 'Sisi :<input type="text" name="sisi"> &nbsp;';
         echo '<label>=</label> &nbsp;';
-        echo '<input type="submit" name="luasPersegi" value="Hitung Luas">';
-        echo '</form>';
-        echo "</br>";
-        echo 'Hitung Keliling Persegi </br>';
-        echo '<form method="POST">';
-        echo 'Sisi :<input type="text" name="sisi"> &nbsp;';
-        echo '<label>=</label> &nbsp;';
-        echo '<input type="submit" name="kelilingPersegi" value="Hitung Keliling">';
+        echo '<input type="submit" name="hitungPersegi" value="Hitung">';
         echo '</form>';
     }
      function FormSegitiga(){
@@ -83,18 +91,19 @@
         echo '<form method="POST">';
         echo 'Alas :<input type="text" name="alas"> &nbsp;';
         echo 'Tinggi :<input type="text" name="tinggi"> &nbsp;';
+        echo 'Sisi Miring :<input type="text" name="miring"> &nbsp;';
         echo '<label>=</label> &nbsp;';
-        echo '<input type="submit" name="luasSegitiga" value="Hitung Luas">';
+        echo '<input type="submit" name="hitungSegitiga" value="Hitung Segitiga">';
         echo '</form>';
     }
      function FormJajargenjang(){
         echo "</br>";
-        echo 'Hitung Luas Jajargenjang </br>';
+        echo 'Hitung Jajargenjang </br>';
         echo '<form method="POST">';
         echo 'panjang :<input type="text" name="panjang"> &nbsp;';
         echo 'lebar :<input type="text" name="lebar"> &nbsp;';
         echo '<label>=</label> &nbsp;';
-        echo '<input type="submit" name="luasJajargenjang" value="Hitung Luas">';
+        echo '<input type="submit" name="hitungJajargenjang" value="Hitung Luas">';
         echo '</form>';
     }
      function FormLayang(){
@@ -104,7 +113,7 @@
         echo 'Diagonal1 :<input type="text" name="diagonal1"> &nbsp;';
         echo 'Diagonal2 :<input type="text" name="diagonal2"> &nbsp;';
         echo '<label>=</label> &nbsp;';
-        echo '<input type="submit" name="luasLayang" value="Hitung Luas">';
+        echo '<input type="submit" name="hitungLayang" value="Hitung Layang">';
         echo '</form>';
     }
 
@@ -114,8 +123,12 @@
         echo '<form method="POST">';
         echo 'Jumlah Sisi Sejajar :<input type="text" name="sisiSejajar"> &nbsp;';
         echo 'Tinggi :<input type="text" name="tinggi"> &nbsp;';
+        echo 'Sisi 1 :<input type="text" name="s1"> &nbsp;';
+        echo 'Sisi 2 :<input type="text" name="s2"> &nbsp;';
+        echo 'Sisi 3 :<input type="text" name="s3"> &nbsp;';
+        echo 'Sisi 4 :<input type="text" name="s4"> &nbsp;';
         echo '<label>=</label> &nbsp;';
-        echo '<input type="submit" name="luasTrapesium" value="Hitung Luas">';
+        echo '<input type="submit" name="HitungTrapesium" value="Hitung Luas">';
         echo '</form>';
     }
      function FormLingkaran(){
@@ -123,7 +136,64 @@
         echo 'Hitung Luas Lingkaran </br>';
         echo '<form method="POST">';
         echo 'Jari-jari :<input type="text" name="jari"> &nbsp;';
-        echo '<input type="submit" name="luasLingkaran" value="Hitung Luas">';
+        echo '<input type="submit" name="hitungLingkaran" value="Hitung Luas">';
+        echo '</form>';
+    }
+     function FormBola(){
+        echo "</br>";
+        echo 'Hitung Bola</br>';
+        echo '<form method="POST">';
+        echo 'Jari-jari :<input type="text" name="jari"> &nbsp;';
+        echo '<input type="submit" name="hitungBola" value="Hitung Bola">';
+        echo '</form>';
+    }
+     function FormTabung(){
+        echo "</br>";
+        echo 'Hitung Tabung</br>';
+        echo '<form method="POST">';
+        echo 'Jari-jari :<input type="text" name="jari"> &nbsp;';
+        echo 'Tinggi :<input type="text" name="tinggi"> &nbsp;';
+        echo '<input type="submit" name="hitungTabung" value="Hitung Tabung">';
+        echo '</form>';
+    }
+     function FormKerucut(){
+        echo "</br>";
+        echo 'Hitung Kerucut</br>';
+        echo '<form method="POST">';
+        echo 'Jari-jari :<input type="text" name="jari"> &nbsp;';
+        echo 'Tinggi :<input type="text" name="tinggi"> &nbsp;';
+        echo 'Selimut :<input type="text" name="selimut"> &nbsp;';
+        echo '<input type="submit" name="hitungKerucut" value="Hitung Kerucut">';
+        echo '</form>';
+    }
+	   function FormKubus(){
+        echo "</br>";
+        echo 'Hitung Kubus </br>';
+        echo '<form method="POST">';
+        echo 'Sisi :<input type="text" name="sisi"> &nbsp;';
+        echo '<label>=</label> &nbsp;';
+        echo '<input type="submit" name="hitungKubus" value="Hitung Kubus">';
+        echo '</form>';
+    }
+	   function FormLimasSegiEmpat(){
+        echo "</br>";
+        echo 'Hitung Limas </br>';
+        echo '<form method="POST">';
+        echo 'Sisi Alas:<input type="text" name="sisia"> &nbsp; </br>';
+        echo 'Sisi Miring:<input type="text" name="sisib"> &nbsp;';
+        echo '<label>=</label> &nbsp;';
+        echo '<input type="submit" name="hitungLimas" value="Hitung Limas">';
+        echo '</form>';
+    }
+	   function FormPrismaSegitiga(){
+        echo "</br>";
+        echo 'Hitung Prima Segitiga </br>';
+        echo '<form method="POST">';
+        echo 'lebar Alas:<input type="text" name="alasa"> &nbsp; </br>';
+        echo 'tinggi Alas:<input type="text" name="tinggia"> &nbsp;';
+        echo 'tinggi Prisma:<input type="text" name="tinggip"> &nbsp;';
+        echo '<label>=</label> &nbsp;';
+        echo '<input type="submit" name="hitungPrisma" value="Hitung Prima Segitiga">';
         echo '</form>';
     }
     if (isset($_GET["page"])&&$_GET!=="") {
@@ -137,11 +207,23 @@
 	    }elseif ($page==="lingkaran") {
 	    FormLingkaran();
 	    }elseif ($page==="jajargenjang") {
-	    ormJajargenjang();
+	    FormJajargenjang();
 	    }elseif ($page==="layang") {
 	    FormLayang();
 	    }elseif ($page==="trapesium") {
 	    FormTrapesium();
+	    }elseif ($page==="bola") {
+	    FormBola();
+	    }elseif ($page==="kerucut") {
+	    FormKerucut();
+	    }elseif ($page==="kubus") {
+	    FormKubus();
+	    }elseif ($page==="limasSegiEmpat") {
+	    FormLimasSegiEmpat();
+	    }elseif ($page==="prismaSegitiga") {
+	    FormPrismaSegitiga();
+	    }elseif ($page==="tabung") {
+	    FormTabung();
 	    }
 	}
 	if (isset($_POST["proses"])) {
@@ -157,17 +239,91 @@
 	  } elseif ($_POST['operasi'] == "bagi") {
 	    echo "Hasil ".$x." / ".$y." = ".$data->Bagi($x, $y)."<br /><br />";
 	  }
-	}elseif (isset($_POST["luasPersegi"])) {
-	  $data2= new Persegi();
+	}elseif (isset($_POST["hitungPersegi"])) {
+	  $obj= new Persegi();
 	  $sisi=$_POST['sisi'];
-	  echo "Luas Persegi :".$data2->Luas($sisi);
-	}elseif (isset($_POST["kelilingPersegi"])) {
-	  $data2= new Persegi();
-	  $sisi=$_POST['sisi'];
-	  echo "Keliling Persegi :".$data2->Keliling($sisi);
-	}elseif (isset($_POST["luasSegitiga"])) {
-	  $data2= new Segitiga();
+	  echo "Luas Persegi :".$obj->Luas($sisi);
+	  echo "<br>";
+	  echo "Keliling Persegi :".$obj->Keliling($sisi);
+	}elseif (isset($_POST["hitungSegitiga"])) {
+	  $obj= new Segitiga();
 	  $alas=$_POST['alas'];
 	  $tinggi=$_POST['tinggi'];
-	  echo "Luas Segitiga :".$data2->Luas($alas,$tinggi);
+	  $miring=$_POST['miring'];
+	  echo "Luas Segitiga :".$obj->Luas($alas,$tinggi);
+	  echo "<br>";
+	  echo "Keliling Segitiga :".$obj->Keliling($alas,$tinggi,$miring);
+	}elseif (isset($_POST["hitungJajargenjang"])) {
+	  $obj= new Jajargenjang();
+	  $panjang=$_POST['panjang'];
+	  $lebar=$_POST['lebar'];
+	  echo "Luas Jajargenjang :".$obj->Luas($panjang,$lebar);
+	  echo "<br>";
+	  echo "Keliling Jajargenjang :".$obj->Keliling($panjang,$lebar);
+	}elseif (isset($_POST["hitungLingkaran"])) {
+	  $obj= new Lingkaran();
+	  $jari=$_POST['jari'];
+	  echo "Luas Lingkaran :".$obj->Luas($jari);
+	  echo "<br>";
+	  echo "Keliling Lingkaran :".$obj->Keliling($jari);
+	}elseif (isset($_POST["hitungTrapesium"])) {
+	  $obj= new Trapesium();
+	  $sisiSejajar=$_POST['sisiSejajar'];
+	  $tinggi=$_POST['tinggi'];
+	  $s1=$_POST['s1'];
+	  $s2=$_POST['s2'];
+	  $s3=$_POST['s3'];
+	  $s4=$_POST['s4'];
+	  echo "Luas Trapesium :".$obj->Luas($sisiSejajar,$tinggi);
+	  echo "<br>";
+	  echo "Keliling Trapesium :".$obj->Keliling($s1,$s2,$s3,$s4);
+	}elseif (isset($_POST["hitungLayang"])) {
+	  $obj= new Layang();
+	  $d1=$_POST['diagonal1'];
+	  $d2=$_POST['diagonal2'];
+	  echo "Luas Layang :".$obj->Luas($d1,$d2);
+	  echo "<br>";
+	  echo "Keliling Layang :".$obj->Keliling($d1,$d2);
+	}elseif (isset($_POST["hitungBola"])) {
+	  $obj= new Bola();
+	  $jari=$_POST['jari'];
+	  echo "Luas permukaan Lingkaran :".$obj->Luas($jari);
+	  echo "<br>";
+	  echo "Volume Lingkaran :".$obj->Volume($jari);
+	}elseif (isset($_POST["hitungKerucut"])) {
+	  $obj= new Kerucut();
+	  $jari=$_POST['jari'];
+	  $selimut=$_POST['selimut'];
+	  $tinggi=$_POST['tinggi'];
+	  echo "Luas permukaan Kerucut :".$obj->Luas($jari,$selimut);
+	  echo "<br>";
+	  echo "Volume Kerucut :".$obj->Volume($jari,$tinggi);
+	}elseif (isset($_POST["hitungTabung"])) {
+	  $obj= new Tabung();
+	  $jari=$_POST['jari'];
+	  $tinggi=$_POST['tinggi'];
+	  echo "Luas permukaan Tabung :".$obj->Luas($jari,$tinggi);
+	  echo "<br>";
+	  echo "Volume Tabung :".$obj->Volume($jari,$tinggi);
+	}elseif (isset($_POST["hitungKubus"])) {
+	  $obj= new Kubus();
+	  $sisi=$_POST['sisi'];
+	  echo "Luas Permukaan Kubus :".$obj->LuasP($sisi);
+	  echo "<br>";
+	  echo "Volume Kubus :".$obj->Volume($sisi);
+	}elseif (isset($_POST["hitungLimas"])) {
+	  $obj= new LimasSegiEmpat();
+	  $sisia=$_POST['sisia'];
+	  $sisib=$_POST['sisib'];
+	  echo "Luas Permukaan Limas :".$obj->Luas($sisia,$sisib);
+	  echo "<br>";
+	  echo "Volume Limas :".$obj->Volume($sisia,$sisib);
+	}elseif (isset($_POST["hitungPrisma"])) {
+	  $obj= new PrismaSegitiga();
+	  $alasa=$_POST['alasa'];
+	  $tinggia=$_POST['tinggia'];
+	  $tinggip=$_POST['tinggip'];
+	  echo "Luas Permukaan Prisma :".$obj->Luas($alasa,$tinggia,$tinggip);
+	  echo "<br>";
+	  echo "Volume Prisma :".$obj->Volume($alasa,$tinggia,$tinggip);
 	}
